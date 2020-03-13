d1f64e9dd6aa00a6f4a4d0bec68736194034f833
Jerimiah Mair <jerimiahmair@gmail.com>
2020-03-13 05:23:22 -0600

    get each players team name

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -5,8 +5,11 @@ import { Container, Title } from "./styles";
 
 const Main = () => {
   const [players, setPlayers] = useState([]);
+  const [teams, setTeams] = useState([]);
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    fetch(`http://localhost:3008/players`, {
+    fetch(`http://localhost:3008/players?_page=${page}&_limit=10`, {
       method: "GET",
       headers: new Headers({
         Accept: "application/json"
@@ -17,15 +20,37 @@ const Main = () => {
         setPlayers(json);
       })
       .catch(error => console.log(error));
-  }, []);
+
+    fetch(`http://localhost:3008/teams`, {
+      method: "GET",
+      headers: new Headers({
+        Accept: "application/json"
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        setTeams(json);
+      })
+      .catch(error => console.log(error));
+  }, [page]);
+
+  const getTeamName = id => {
+    return teams.filter(team => team.id === id)[0].name;
+  };
 
   return (
     <Container>
       <Title>NBA Interview</Title>
       <Search />
-      {players &&
+      {players.length &&
+        teams.length &&
         players.map(player => (
-          <Card name={player.name} image={player.image} team={player.team} />
+          <Card
+            key={player.name}
+            name={player.name}
+            image={player.image}
+            team={getTeamName(player.team)}
+          />
         ))}
     </Container>
   );
