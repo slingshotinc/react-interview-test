f1ab04661dd68c1ba15a9dbb735e2e098f1690d7
Jerimiah Mair <jerimiahmair@gmail.com>
2020-03-13 06:05:38 -0600

    adjust styles

--- src/components/Main/Card/styles.js
+++ src/components/Main/Card/styles.js
@@ -1,22 +1,35 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-	background-color: #f2f2f2;
-	border: 1px solid #EEE;
-	border-radius: 4px;
-	padding: 16px;
-	width: 280px;
-	text-align: center;
-`
+  color: white;
+  background-color: #f2f2f2;
+  border-radius: 4px;
+  border: 2px solid #515252;
+  padding: 16px 0 0;
+  width: 280px;
+  text-align: center;
+  margin: 12px 0;
+  box-shadow: inset 0 0 100px 90px black, 0 4px 14px black;
+`;
 
 export const Name = styled.p`
-	padding-bottom: 16px;
-`
+  padding-bottom: 16px;
+  font-size: 24px;
+  margin: 12px 0 0;
+`;
 
-export const PlayerImg = styled.img`
-	padding-bottom: 16px;
-`
+export const PlayerImg = styled.img``;
 
 export const Team = styled.p`
-	
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  color: black;
+  margin: 0;
+  height: 48px;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  box-shadow: inset 0 6px 24px 0 #868282;
+  font-weight: 800;
+`;
--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-	height: 100%;
-	width: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  justify-contents: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  font-family: "Roboto", sans-serif;
+`;
 
 export const Title = styled.p`
-	font-size: 24px;
-	padding-bottom: 32px;
- 	text-align: center;
-`
+  font-size: 24px;
+  padding-bottom: 32px;
+  text-align: center;
+  color: white;
+`;
