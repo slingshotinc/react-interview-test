ce78d427b9c8248bb2ef25c24aaef8d657e23700
Jerimiah Mair <jerimiahmair@gmail.com>
2020-03-13 04:55:29 -0600

    retreive list and render card for each player

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./Search";
 import Card from "./Card";
 import { Container, Title } from "./styles";
 
-const Main = () => (
-  <Container>
-    <Title>NBA Interview</Title>
-    <Search />
-    <Card />
-  </Container>
-)
+const Main = () => {
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:3008/players`, {
+      method: "GET",
+      headers: new Headers({
+        Accept: "application/json"
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        setPlayers(json);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <Container>
+      <Title>NBA Interview</Title>
+      <Search />
+      {players &&
+        players.map(player => (
+          <Card name={player.name} image={player.image} team={player.team} />
+        ))}
+    </Container>
+  );
+};
 
 export default Main;
-
